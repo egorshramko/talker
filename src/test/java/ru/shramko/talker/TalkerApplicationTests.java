928a3cb2ab372c0b298f40b3782fb7c1077ca521
Egor Shramko <egorii2001@gmail.com>
2025-11-15 19:39:48 +0700

    Initial commit

---
+++ src/test/java/ru/shramko/talker/TalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shramko.talker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
