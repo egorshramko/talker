87e38fd5db19c4db472693837193fb9287e6316b
Egor Shramko <egorii2001@gmail.com>
2025-11-16 21:16:28 +0700

    feat(posts): opening posts on main page

--- src/main/java/ru/shramko/talker/controllers/PostController.java
+++ src/main/java/ru/shramko/talker/controllers/PostController.java
@@ -1,11 +1,15 @@
 package ru.shramko.talker.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -50,6 +54,25 @@ public class PostController {
 		postRepository.save(post);
 		
 		return "redirect:/posts";
+	}
+	
+	@GetMapping("/post/{id}")
+	public String getExistsPost(@PathVariable Long id,
+			Model model) {
+		
+		Post opensPost = postRepository.findById(id)
+				.orElse(null);
+		
+		if (opensPost != null) {
+			model.addAttribute("post", opensPost);
+			return "post";
+		}
+		else {
+			throw new ResponseStatusException(
+					HttpStatus.NOT_FOUND, 
+					"Post with id " + id.toString() + " does not exist");
+		}
+		
 	}
 	
 }
