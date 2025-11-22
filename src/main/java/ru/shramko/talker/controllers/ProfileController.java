31172f7d7090e28870dab0de7c14f3599a40a1fc
Egor Shramko <egorii2001@gmail.com>
2025-11-22 15:36:14 +0700

    chore(ProfileController.java): add profile controller

---
+++ src/main/java/ru/shramko/talker/controllers/ProfileController.java
@@ -0,0 +1,17 @@
+package ru.shramko.talker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
+public class ProfileController {
+
+	
+	@GetMapping
+	public String profilePage() {
+		
+	}
+	
+}
