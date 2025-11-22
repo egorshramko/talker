a69f7cdc94e31e7fa5bd094191faaef7aab54c0d
Egor Shramko <egorii2001@gmail.com>
2025-11-22 16:49:07 +0700

    chore(ProfileController.java): add loading profile to model

--- src/main/java/ru/shramko/talker/controllers/ProfileController.java
+++ src/main/java/ru/shramko/talker/controllers/ProfileController.java
@@ -1,17 +1,24 @@
 package ru.shramko.talker.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ru.shramko.talker.service.ProfileService;
+
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
 
+	@Autowired
+	private ProfileService profileService;
+	
 	@GetMapping("/{id}")
-	public String profilePage(@PathVariable Long id) {
+	public String profilePage(@PathVariable Long id, Model model) {
+		model.addAttribute("profile", profileService.getProfileByUserId(id));
 		return "profile";
 	}
 	
