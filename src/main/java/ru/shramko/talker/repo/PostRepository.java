62ad80970bde1ca01ee7407d886633cf39d9f1d9
Egor Shramko <egorii2001@gmail.com>
2025-11-22 17:36:40 +0700

    chore(PostRepository.java): add method findAllByAuthor

--- src/main/java/ru/shramko/talker/repo/PostRepository.java
+++ src/main/java/ru/shramko/talker/repo/PostRepository.java
@@ -1,11 +1,14 @@
 package ru.shramko.talker.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import ru.shramko.talker.data.Post;
+import ru.shramko.talker.security.data.User;
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
-
+	List<Post> findAllByAuthor(User author);
 }
