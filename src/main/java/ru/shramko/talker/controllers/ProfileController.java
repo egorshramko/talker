3eb207d6b1abf2a7e869b11befde62bf10f0020e
Egor Shramko <egorii2001@gmail.com>
2025-11-22 15:50:28 +0700

    chore(ProfileController.java): add id to profile page path

--- src/main/java/ru/shramko/talker/controllers/ProfileController.java
+++ src/main/java/ru/shramko/talker/controllers/ProfileController.java
@@ -1,17 +1,18 @@
 package ru.shramko.talker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
 
-	
-	@GetMapping
-	public String profilePage() {
-		
+	@GetMapping("/{id}")
+	public String profilePage(@PathVariable Long id) {
+		return "profile";
 	}
 	
 }
