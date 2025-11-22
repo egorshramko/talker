6a30ced2bb0a8ff3a1c2703032db54bbc3639b3f
Egor Shramko <egorii2001@gmail.com>
2025-11-22 17:19:01 +0700

    fix(ProfileController.java): fix current user load

--- src/main/java/ru/shramko/talker/controllers/ProfileController.java
+++ src/main/java/ru/shramko/talker/controllers/ProfileController.java
@@ -1,12 +1,15 @@
 package ru.shramko.talker.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ru.shramko.talker.security.data.User;
 import ru.shramko.talker.service.ProfileService;
 
 @Controller
@@ -15,6 +18,11 @@ public class ProfileController {
 
 	@Autowired
 	private ProfileService profileService;
+	
+	@ModelAttribute("user")
+	public User user(@AuthenticationPrincipal User user) {
+		return user;
+	}
 	
 	@GetMapping("/{id}")
 	public String profilePage(@PathVariable Long id, Model model) {
