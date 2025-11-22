10b2aade36ffb8edeb188341f4d80d8d6666e6b3
Egor Shramko <egorii2001@gmail.com>
2025-11-22 17:38:12 +0700

    chore(ProfileServiceImpl.java): add user posts to profile

--- src/main/java/ru/shramko/talker/service/impl/ProfileServiceImpl.java
+++ src/main/java/ru/shramko/talker/service/impl/ProfileServiceImpl.java
@@ -3,9 +3,11 @@ package ru.shramko.talker.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.shramko.talker.data.Profile;
+import ru.shramko.talker.repo.PostRepository;
 import ru.shramko.talker.repo.ProfileRepository;
 import ru.shramko.talker.security.repo.UserRepository;
 import ru.shramko.talker.service.ProfileService;
@@ -20,14 +22,22 @@ public class ProfileServiceImpl implements ProfileService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
+	@Transactional
 	public Profile getProfileByUserId(Long userId) {
-		return profileRepository.getProfileByUserId(userId)
+		Profile profile = profileRepository.getProfileByUserId(userId)
 				.orElse(new Profile(userRepository
 						.findById(userId)
 						.orElseThrow(() -> 
 								new UsernameNotFoundException(
 										"User with id " + userId.toString() + " not found"))));
+		
+		profile.setPosts(postRepository.findAllByAuthor(profile.getUser()));
+		
+		return profile;
 				
 	}
 
